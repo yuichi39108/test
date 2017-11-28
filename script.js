c6efd2fcca60d7a54c849113d7ab88bf1af2c5ac
yuichi39108 <0824bibby@gmail.com>
2017-11-28 14:19:03 +0900

    処理の発火をイベントリスナーに登録

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+// boxのDOMを取得
 var box = document.getElementById('box');
+// btnのDOMを取得
 var btn = document.getElementById('btn');
 
 function changeBackgroundColor(){
   box.classList.toggle('red');
 }
+
+btn.addEventListener('click', changeBackgroundColor);
+
+//(例) btn.addEventListener('click', function(){
+  // console.log('クリックしたらボックスの色が変わるよ');
+// });
